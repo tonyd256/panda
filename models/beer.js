fe26ed679153557cf2520bfe72cae38b4c86b5a3
Andrew Hammond <fenix.mercury@gmail.com>
2012-06-19 01:39:09 +0000

    added comment about beers...

--- models/beer.js
+++ models/beer.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// other possible attributes:
+// + ABV
+// + volume per container (12oz bottle vs 16oz bottle)
+// other thoughts:
+// + should we separate out beer categories? (into it's own table)
+//   ... this would be so we could have status and recommend beers by type
 var sBeer = new Schema({
   name: { type: String, required: true },
   style: { type: String, required: true },
