11fbe2b942dc507b20710585542dd58c371ee83e
Andrew Hammond <fenix.mercury@gmail.com>
2012-06-19 01:26:45 +0000

    added comment about inventory...

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -13,6 +13,13 @@ exports.inventory = {
       model.find({}, cb);
     }
   },
+  // should start thinking about which slots of the machine
+  // are loaded with the particular type of beer...
+  // might need a separate endpoint to reverse lookup the 
+  // inventory based on the index of a matrix of vendor spots...
+  // (we'll need to have a way to know which type of beer was 
+  // just vended if somebody takes out a beer from slot A6 
+  // for example...)
   post: function (data, cb) {
     var b = new model({
       beer: data.beer,
